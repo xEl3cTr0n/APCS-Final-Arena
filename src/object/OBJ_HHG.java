91c3089c12a34ccfdfb35ce15099c1c2d4a0d543
xEl3cTr0n <aarhakhanna@gmail.com>
2022-05-24 23:29:41 -0700

    added grenade

--- src/object/OBJ_HHG.java
+++ src/object/OBJ_HHG.java
@@ -7,13 +7,13 @@ import javax.imageio.ImageIO;
 import main.GamePanel;
 
 
-public class OBJ_heal extends SuperObject {
+public class OBJ_HHG extends SuperObject {
 	GamePanel gp;
-	public OBJ_heal(GamePanel gp) {
+	public OBJ_HHG(GamePanel gp) {
 		this.gp = gp;
-		name = "Heal";
+		name = "Holy Hand Grenade";
 		try {
-			image = ImageIO.read(getClass().getResourceAsStream("/objects/heal.png"));
+			image = ImageIO.read(getClass().getResourceAsStream("/objects/HHG.png"));
 			uTool.scaleImage(image, gp.tileSize, gp.tileSize);
 		} catch(IOException e) {
 			e.printStackTrace();
